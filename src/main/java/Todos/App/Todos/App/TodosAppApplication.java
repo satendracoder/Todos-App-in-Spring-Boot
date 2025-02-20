be7ddad076a181e819015acb39f5113c85d04a81
satendrcoder <satendracaria@gmail.com>
2025-02-20 12:49:10 +0530

    Hello Java Tods App

---
+++ src/main/java/Todos/App/Todos/App/TodosAppApplication.java
@@ -0,0 +1,13 @@
+package Todos.App.Todos.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodosAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodosAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Todos/App/Todos/App/TodosAppApplicationTests.java
@@ -0,0 +1,13 @@
+package Todos.App.Todos.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodosAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
